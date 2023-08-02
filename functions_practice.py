72123dfe3fe78a0f0385da94150b92a5114d581e
Rashonda Edwards <rashondaedwards@aol.com>
2023-08-02 11:02:43 -0700

    Final commit

--- functions_practice.py
+++ functions_practice.py
@@ -1,3 +1,22 @@
 def hello():
     print("Hello user person!")
 
+def pack(a,b,c):
+    return [a, b, c]
+
+def eat_lunch(alist):
+    if not alist:
+        print("My lunchbox is empty!")
+    else:
+        print("First I eat", alist[0])
+        for food in alist[1:]:
+            print("Next I eat", food)
+
+
+
+hello()
+
+result = pack("Lions", "Tigers", "Bears")
+print(result)
+
+eat_lunch(["grapes", "bananas", "apples", "watermelon"])
